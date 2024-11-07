93abad3430572cfe240e24323eeaa26463ca2b6f
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-11-07 20:27:20 +0530

    added dockerignore

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>This is just a testing app for learning Docker . {count} </p>
+
+        <a className="App-link" onClick={() => setCount((prev) => prev + 1)}>
+          increase
         </a>
       </header>
     </div>
